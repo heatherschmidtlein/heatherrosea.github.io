a98ba86b140b36c908d5db2348da40613f8ad52d
heatherrosea <71243785+heatherrosea@users.noreply.github.com>
2020-10-10 22:58:04 -0600

    Create preston-js.js

---
+++ preston-js.js
@@ -0,0 +1,44 @@
+
+const daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+const d = new Date();
+const dayName = daynames[d.getDay()];
+const monthName = months[d.getMonth()];
+const todaydate = dayName + ", " + d.getDate() + " " + monthName + " " + d.getFullYear();
+
+document.getElementById('date').textContent = todaydate;
+document.getElementById("year").innerHTML = d.getFullYear();
+
+
+//
+
+
+const hambutton = document.querySelector('.menu');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+
+//
