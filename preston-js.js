6ae5cea8b25f5ed383985dbd6c82c5a2c3101075
heatherrosea <71243785+heatherrosea@users.noreply.github.com>
2021-01-30 16:35:20 -0700

    Update preston-js.js

--- preston-js.js
+++ preston-js.js
@@ -41,4 +41,6 @@ const mainnav = document.querySelector('.navigation')
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 
 
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
 //
