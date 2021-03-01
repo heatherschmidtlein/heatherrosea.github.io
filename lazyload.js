be589240c1b64830bf5ed3b0baefbf1963489d21
heatherrosea <71243785+heatherrosea@users.noreply.github.com>
2021-02-28 22:20:33 -0700

    Update lazyload.js

--- lazyload.js
+++ lazyload.js
@@ -1,28 +1,33 @@
 const imagesToLoad = document.querySelectorAll('img[data-src]');
 
-const imgOptions = {
-    threshold: 1,
-    rootMargin: "0px 0px 50px 0px"
-};
-
 const loadImages = (image) => {
   image.setAttribute('src', image.getAttribute('data-src'));
-  image.onload = () => {image.removeAttribute('data-src');};
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
 };
 
-if('IntersectionObserver' in window) {
-  const imgObserver = new IntersectionObserver((items, observer) => {
+const imgOptions = {
+  threshold:1,
+  rootMargin:'0px 0px 50px 0px'
+};
+
+if ('IntersectionObserver' in window) {
+  const imgObserver = new IntersectionObserver ((items) => {
     items.forEach((item) => {
-      if(item.isIntersecting) {
-          loadImages(item.target);
-          observer.unobserve(item.target);
-        }
-      });
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        imgObserver.unobserve(item.target);
+      }
     });
-    imagesToLoad.forEach((img) => {
-      imgObserver.observe(img);});
- 
-} else {
-    imagesToLoad.forEach((img) => {
-      loadImages(img);});
-  }
+  }, imgOptions);
+
+  imagesToLoad.forEach((img) => {
+    imgObserver.observe(img);
+  });
+}
+else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
