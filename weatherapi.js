07c2b75bf8cd1d9aabe0ce99602cbe16971752dd
heatherrosea <71243785+heatherrosea@users.noreply.github.com>
2021-03-20 13:41:35 -0600

    Update weatherapi.js

--- weatherapi.js
+++ weatherapi.js
@@ -9,6 +9,22 @@ fetch(apiURL)
   document.getElementById('temperature').textContent = jsObject.main.temp;
   document.getElementById('humidity').textContent = jsObject.main.humidity;
   document.getElementById('wind-speed').textContent = jsObject.wind.speed;
+  
+  
+let t = parseFloat(document.getElementById('temperature').textContent);
+let s = parseFloat(document.getElementById('wind-speed').textContent);
+
+let wc = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
+
+if (t <= 50 && s > 3) {
+     wc = Math.round(wc);
+  } 
+else {
+     wc = "N/A";
+  }
+
+document.getElementById('wind-chill').innerHTML = wc;
+
 
   
   });
